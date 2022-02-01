4e709c0783ec3763659aed724cfdbbb14d537b53
monochromegane <dev.kuro.obi@gmail.com>
2022-02-01 10:32:38 +0900

    Add epsilon greedy implementaion and few tests.

--- banditalgorithms/epsilon_greedy.py
+++ banditalgorithms/epsilon_greedy.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional, cast
 
 import numpy as np
 
@@ -7,12 +7,29 @@ class EpsilonGreedy:
     def __init__(
         self, num_arms: int, *, epsilon: float = 0.1, seed: Optional[int] = None
     ) -> None:
+        self.random = np.random.RandomState(seed)
+
         self.num_arms = num_arms
         self.epsilon = epsilon
-        self.rs = np.random.RandomState(seed)
+
+        self.counts: List[float] = [0.0 for _ in range(self.num_arms)]
+        self.rewards: List[float] = [0.0 for _ in range(self.num_arms)]
 
     def select(self) -> int:
-        ...
+        if self._is_exploitation():
+            return cast(int, np.argmax(self._theta_hats()))
+        else:
+            return self.random.choice(len(self._theta_hats()))
 
     def update(self, idx_arm: int, reward: float) -> None:
-        ...
+        self.counts[idx_arm] += 1.0
+        self.rewards[idx_arm] += reward
+
+    def _is_exploitation(self) -> bool:
+        return self.random.random() > self.epsilon
+
+    def _theta_hats(self) -> List[float]:
+        return [
+            reward / count if count > 0.0 else 0.0
+            for count, reward in zip(self.counts, self.rewards)
+        ]
--- tests/test_epsilon_greedy.py
+++ tests/test_epsilon_greedy.py
@@ -1,8 +1,40 @@
 from banditalgorithms import epsilon_greedy
 
 
-def test_epsilon_greedy() -> None:
+def test_epsilon_greedy_update() -> None:
+    rewards = [10.0, 20.0, 30.0]
+    cum_rewards = [10.0, 30.0, 60.0]
+    idx_arm = 0
+
     algo = epsilon_greedy.EpsilonGreedy(1)
-    selected = algo.select()
-    algo.update(selected, 1.0)
-    assert True
+
+    assert algo.counts[idx_arm] == 0.0
+    assert algo.rewards[idx_arm] == 0.0
+
+    for i in range(len(rewards)):
+        reward = rewards[i]
+        cum_reward = cum_rewards[i]
+
+        algo.update(idx_arm, reward)
+
+        assert algo.counts[idx_arm] == i + 1.0
+        assert algo.rewards[idx_arm] == cum_reward
+
+
+def test_epsilon_greedy_theta_hats() -> None:
+    num_arms = 3
+    algo = epsilon_greedy.EpsilonGreedy(num_arms)
+    rewards = [10.0, 20.0, 30.0]
+    idx_arm = 0
+    for reward in rewards:
+        algo.update(idx_arm, reward)
+
+    theta_hats = algo._theta_hats()
+    assert len(theta_hats) == num_arms
+    assert theta_hats[idx_arm] == sum(rewards) / len(rewards)
+
+
+def test_epsilon_greedy_theta_hats_with_zero_counts() -> None:
+    algo = epsilon_greedy.EpsilonGreedy(1)
+    theta_hats = algo._theta_hats()
+    assert theta_hats[0] == 0.0
