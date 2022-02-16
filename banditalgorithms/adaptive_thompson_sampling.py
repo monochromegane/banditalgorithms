7432ffa871905127e93c59a056de33868e4f5a71
monochromegane <dev.kuro.obi@gmail.com>
2022-02-16 10:37:54 +0900

    Add estimator of AdTS.

--- banditalgorithms/adaptive_thompson_sampling.py
+++ banditalgorithms/adaptive_thompson_sampling.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, cast
 
 import numpy as np
 
@@ -15,9 +15,25 @@ class AdaptiveThompsonSampling:
 
         self.num_arms = num_arms
         self.dim_context = dim_context
+        self.estimators = [AdaptiveThompsonSamplingEstimator() for _ in range(num_arms)]
 
     def select(self, ctx: List[float]) -> int:
-        ...
+        x = np.c_[np.array(ctx)]
+        return cast(
+            int, np.argmax([estimator.estimate(x) for estimator in self.estimators])
+        )
 
     def update(self, idx_arm: int, reward: float, ctx: List[float]) -> None:
+        x = np.c_[np.array(ctx)]
+        self.estimators[idx_arm].update(reward, x)
+
+
+class AdaptiveThompsonSamplingEstimator:
+    def __init__(self) -> None:
+        ...
+
+    def estimate(self, x: np.ndarray) -> float:
+        ...
+
+    def update(self, reward: float, x: np.ndarray) -> float:
         ...
