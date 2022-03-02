c0376af00af16d312aff204bc37d92cfb963e0c3
monochromegane <dev.kuro.obi@gmail.com>
2022-03-02 12:55:54 +0900

    Add badness for change detection to dLinUCB

--- banditalgorithms/dynamic_linucb.py
+++ banditalgorithms/dynamic_linucb.py
@@ -2,6 +2,7 @@ import math
 from typing import List, Optional, cast
 
 import numpy as np
+from scipy import special
 
 from . import inverse_matrix as mat
 
@@ -17,6 +18,7 @@ class DynamicLinUCB:
         delta2: float = 0.1,
         delta1_tilde: Optional[float] = None,
         sigma2: float = 1e-2,
+        tau: int = 10,
     ) -> None:
         self.num_arms = num_arms
         self.dim_context = dim_context
@@ -26,6 +28,7 @@ class DynamicLinUCB:
         self.delta2 = delta2
         self.delta1_tilde = delta1_tilde
         self.sigma2 = sigma2
+        self.tau = tau
 
         self.models = [
             DynamicLinUCBSlave(
@@ -46,8 +49,8 @@ class DynamicLinUCB:
 
     def update(self, idx_arm: int, reward: float, ctx: List[float]) -> None:
         x = np.c_[np.array(ctx)]
-        idx_model = 0
-        self.models[idx_model].update(idx_arm, reward, x)
+        for idx_model in range(len(self.models)):
+            self.models[idx_model].update(idx_arm, reward, x)
 
 
 class DynamicLinUCBSlave:
@@ -61,6 +64,7 @@ class DynamicLinUCBSlave:
         delta2: float = 0.1,
         delta1_tilde: Optional[float] = None,
         sigma2: float = 1e-2,
+        tau: int = 10,
     ) -> None:
         self.num_arms = num_arms
         self.dim_context = dim_context
@@ -71,29 +75,48 @@ class DynamicLinUCBSlave:
         if delta1_tilde is None:
             self.delta1_tilde = delta1
         self.sigma2 = sigma2
+        self.sigma = math.sqrt(sigma2)
+        self.tau = tau
+        self.epsilon = math.sqrt(2.0) * self.sigma * special.erfinv(1.0 - self.delta1)
 
         self.invAs = [
             mat.InverseMatrix(dim_context, lambda_=lambda_) for _ in range(num_arms)
         ]
         self.bs = [np.zeros([dim_context, 1]) for _ in range(num_arms)]
         self.counts = [0 for _ in range(num_arms)]
+        self.es: List[float] = []
+        self.e_hat = 0.0
+        self.d = 0.0
 
     def select(self, x: np.ndarray) -> int:
         return cast(int, np.argmax([score for score in self._ucb_scores(x)]))
 
     def update(self, idx_arm: int, reward: float, x: np.ndarray) -> None:
+        e = 0.0
+        if (
+            abs(self._reward_hat(idx_arm, x) - reward)
+            > self.B(idx_arm, x) + self.epsilon
+        ):
+            e = 1.0
+        self.es.append(e)
+
         self.bs[idx_arm] += reward * x
         self.invAs[idx_arm].update(x)
         self.counts[idx_arm] += 1
+
+        recently_es = self.es[max(0, len(self.es) - self.tau) :]
+        self.e_hat = sum(recently_es) / len(recently_es)
+        self.d = math.sqrt(math.log(1.0 / self.delta2) / (2 * len(recently_es)))
 
     def _ucb_scores(self, x: np.ndarray) -> List[float]:
         return [self._ucb_score(i, x) for i in range(self.num_arms)]
 
     def _ucb_score(self, idx_arm: int, x: np.ndarray) -> float:
-        theta_hat = self.invAs[idx_arm].data.dot(self.bs[idx_arm])
-        reward_hat = cast(float, x.T.dot(theta_hat)[0][0])
+        return self._reward_hat(idx_arm, x) + self.B(idx_arm, x)
 
-        return reward_hat + self.B(idx_arm, x)
+    def _reward_hat(self, idx_arm: int, x: np.ndarray) -> float:
+        theta_hat = self.invAs[idx_arm].data.dot(self.bs[idx_arm])
+        return cast(float, x.T.dot(theta_hat)[0][0])
 
     def B(self, idx_arm: int, x: np.ndarray) -> float:
         d = self.dim_context
