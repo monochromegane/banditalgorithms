3ae9a3e2a654d5258805b73dbaca51d71f9ef67c
monochromegane <dev.kuro.obi@gmail.com>
2022-03-02 19:24:17 +0900

    Forget assertion of some tests.

--- tests/test_dynamic_linucb.py
+++ tests/test_dynamic_linucb.py
@@ -153,8 +153,8 @@ def test_dynamic_linucb_slave_update_when_confidence_bound_exceed() -> None:
         for reward in rewards:
             algo.update(idx_arm, reward, ctx)
 
-    np.allclose(slave.bs[idx_arm], np.zeros([dim_context, 1]))
-    np.allclose(slave.invAs[idx_arm].data, np.linalg.inv(np.eye(dim_context)))
+    assert np.allclose(slave.bs[idx_arm], np.zeros([dim_context, 1]))
+    assert np.allclose(slave.invAs[idx_arm].data, np.linalg.inv(np.eye(dim_context)))
     assert slave.counts[idx_arm] == 0
 
 
@@ -175,19 +175,21 @@ def test_dynamic_linucb_slave_B() -> None:
     num_arms = 1
     dim_context = 1
     algo = dynamic_linucb.DynamicLinUCB(num_arms, dim_context, sigma2=1.0, delta1=1.0)
+    slave = algo.models[0]
     rewards = [10.0 for _ in range(9)]
     idx_arm = 0
     ctx = [1.0]
     x = np.c_[np.array(ctx)]
 
-    for reward in rewards:
-        algo.update(idx_arm, reward, ctx)
-    slave = algo.models[0]
+    with patch.object(slave, "_exceed_confidence_bound") as mock_exceed:
+        mock_exceed.return_value = False
+        for reward in rewards:
+            algo.update(idx_arm, reward, ctx)
 
     expected_B = (
         0.5746812313093125  # alpha(= 1.8173015965970112) * ||x||_A^{-1}(= 0.31622777)
     )
-    np.allclose(slave.B(idx_arm, x), expected_B)
+    assert np.isclose(slave.B(idx_arm, x), expected_B)
 
 
 def test_dynamic_linucb_slave_badness() -> None:
@@ -210,11 +212,11 @@ def test_dynamic_linucb_slave_badness() -> None:
         expected_d = 0.10264527054756413
         assert len(slave._recently_es()) == len(rewards)
         assert slave.e_hat == 1.0
-        np.allclose(slave.d, expected_d)
+        assert np.allclose(slave.d, expected_d)
 
         mock_slave.return_value = 0.0
         slave.update(idx_arm, reward, x)
 
         assert len(slave._recently_es()) == len(rewards)
         assert slave.e_hat == 0.8
-        np.allclose(slave.d, expected_d)
+        assert np.allclose(slave.d, expected_d)
