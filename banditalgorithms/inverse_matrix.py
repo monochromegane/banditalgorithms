907d19665514d60b460ed5cc77cebdcaab558502
monochromegane <dev.kuro.obi@gmail.com>
2022-12-14 11:18:47 +0900

    Speeds up inverse matrix calculations

--- banditalgorithms/inverse_matrix.py
+++ banditalgorithms/inverse_matrix.py
@@ -4,7 +4,9 @@ import numpy as np
 class InverseMatrix:
     def __init__(self, dim: int, lambda_: float = 1.0) -> None:
         self.dim = dim
-        self._inv = np.linalg.inv(lambda_ * np.eye(dim))
+        self._inv = (1.0 / lambda_) * np.eye(dim)
+        self.identity = np.eye(1)
+        self.out = np.zeros((self.dim, self.dim))
 
     @property
     def data(self) -> np.ndarray:
@@ -13,9 +15,10 @@ class InverseMatrix:
     def update(self, x: np.ndarray) -> None:
         inv = self._inv
 
-        self._inv -= (
-            inv.dot(x)
-            .dot(np.linalg.inv(np.eye(1) + x.T.dot(inv).dot(x)))
-            .dot(x.T)
-            .dot(inv)
+        xTinv = x.T.dot(inv)
+        np.outer(
+            inv.dot(x).dot(np.linalg.inv(self.identity + xTinv.dot(x))),
+            xTinv,
+            out=self.out,
         )  # Woodbury
+        self._inv -= self.out
